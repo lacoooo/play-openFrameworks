3b80bb09c5c7f6c56f4c6b09d26281fa5bbf20d1
neko <469867876@qq.com>
2019-09-08 15:34:57 +0800

    9

--- 001-lines/src/ofApp.cpp
+++ 001-lines/src/ofApp.cpp
@@ -1,8 +1,10 @@
 #include "ofApp.h"
 
+
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	//ofNoFill();
 }
 
 //--------------------------------------------------------------
@@ -12,7 +14,24 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	float cx = ofGetWidth() / 2;
+	float cy = ofGetHeight() / 2;
+	float radius = 300.0;
+	for (int i = 0; i < 100; i+=1) {
+		ofPushMatrix();
+		ofSetColor(ofColor(abs(sin(i + 20.0 + time / 10.0) * 250.0), abs(sin(i + 40.0 + time / 10.0) * 250.0), abs(sin(i + time / 10.0) * 250.0)));
+		ofTranslate(
+			cx + sin((i + time / 10.0) / 4.0) * radius
+			+ sin((i + time / 20.0) * 8.0) * radius / 8.0
+			,
+			cy + cos((i + time / 10.0) / 4.0) * radius
+			+ cos((i + time / 20.0) * 8.0) * radius / 8.0
+		);
+		ofRotate((i + time / 100.0) * 100.0);
+		ofDrawBox(20.0 + sin(i + time / 10.0));
+		ofPopMatrix();
+	}
+	time+= 0.1;
 }
 
 //--------------------------------------------------------------
